c57c4221054a575d9b9af75a51ad84cd1dfcbb3b
Djenica <edromediatech@gmail.com>
2023-08-21 14:13:55 -0500

    nuxt js meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,10 +10,12 @@ export default {
     },
     meta: [
       { charset: 'utf-8' },
+      {name:'description', content:'Eglise Baptiste Jérusalem de Fort-Liberté, https://ebjfl.org'},
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
+    
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
