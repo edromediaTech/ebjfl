f81a6474a65839cd6130640b69162bcc54b3530e
Djenica Rubes <djenicarubes@gmail.com>
2023-11-03 15:46:51 -0400

    init project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+  
+  <Tutorial/>
+ 
+</v-container>
+</template>
+
+<script>
+import header from '../components/header.vue';
+export default {
+  name: 'IndexPage',
+  components :{header}
+}
+</script>
